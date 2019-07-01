90622d90e23723dc4eb9345784baccf80389a015
Anonymous <anonymous@arch.localdomain>
2019-07-01 13:38:52 +0200

    Install script

--- install-script.sh
+++ install-script.sh
@@ -5,8 +5,4 @@ timedatectl set-ntp true &&
 
 wipefs -a /dev/sda &&
 parted /dev/sda mklabel gpt &&
-parted /dev/sda mkpart ext4 1MiB 3MiB && 
-parted /dev/sda mkpart ext4 3MiB 100% &&
-parted /dev/sda set 1 bios_grub on &&
-
-mkfs.ext4 /dev/sda2
+parted /dev/sda mkpart ext4 1MiB 100%
