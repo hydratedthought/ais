9f9b7938b384f685cdd202cb71c5dec0291f4fef
Anonymous <anonymous@arch.localdomain>
2019-07-01 13:25:11 +0200

    Install script

--- install-script.sh
+++ install-script.sh
@@ -4,7 +4,7 @@ loadkeys sv-latin1 &&
 timedatectl set-ntp true &&
 
 wipefs -a /dev/sda &&
-parted /dev/sda mklable gpt &&
+parted /dev/sda mklabel gpt &&
 parted /dev/sda mkpart ext4 1MiB 3MiB && 
 parted /dev/sda mkpart ext4 3MiB 100% &&
 parted /dev/sda set 1 bios_grub on &&
