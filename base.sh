1522f912c8f542e142aa61ecb73cc2324f9436f1
Anonymous <anonymous@arch.localdomain>
2019-07-02 11:53:25 +0200

    Base install

--- base.sh
+++ base.sh
@@ -11,5 +11,18 @@ parted /dev/sda set 1 bios_grub on &&
 mkfs.ext4 /dev/sda2 &&
 
 mount /dev/sda2 /mnt &&
-pacstrap /mnt base base-devel
+pacstrap /mnt base base-devel &&
 
+genfstab -U /mnt >> /mnt/etc/fstab &&
+arch-chroot /mnt &&
+ln -sf /usr/share/zoneinfo/Europe/Stockholm /etc/localtime &&
+hwclock --systohc &&
+
+echo en_US.UTF-8 > /etc/locale.gen &&
+locale-gen &&
+echo LANG=en_US.UTF-8 > /etc/locale.conf &&
+echo KEYMAP=sv-latin1 > /etc/vconsole.conf &&
+echo arch > /etc/hosts &&
+echo 127.0.0.1  localhost >> /etc/hosts &&
+echo ::1        localhost >> /etc/hosts &&
+echo 157.0.1.1  arch.localdomain arch >> /etc/hosts
