3100a7086a46ab294c5a6dd12f8da7f4881afe05
anonymous <anonymous@arch.localdomain>
2019-07-05 16:52:27 +0200

    First script

--- first.sh
+++ first.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
+loadkeys sv-latin1
 timedatectl set-ntp true
+
 wipefs -a /dev/sda 
 parted /dev/sda mklabel gpt
 parted /dev/sda mkpart ext4 1MiB 3MiB
 parted /dev/sda mkpart ext4 3MiB 100%
 parted /dev/sda set 1 bios_grub on
 mkfs.ext4 /dev/sda2
+
 mount /dev/sda2 /mnt
 pacstrap /mnt base base-devel
-genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt
-echo "Chroot successful!"
+
+#genfstab -U /mnt >> /mnt/etc/fstab
+#arch-chroot /mnt
+#echo "Chroot successful!"
